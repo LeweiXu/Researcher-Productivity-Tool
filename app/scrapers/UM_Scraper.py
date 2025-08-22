8c555dbbbfa54eb06c895a596390972dec1f19a6
Jarrah9 <jarrah.oatham@gmail.com>
2025-08-23 00:23:50 +0800

    changed output format and skip repeats

--- app/scrapers/UM_Scraper.py
+++ app/scrapers/UM_Scraper.py
@@ -1,20 +1,27 @@
 from selenium import webdriver
+import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from pyalex import Works, Authors, Institutions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+import time
+import random
 
 def get_staff(url):
     driver = webdriver.Chrome()
     driver.get(url)
     
-    driver.implicitly_wait(5)
+    WebDriverWait(driver, 8).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
     
     name_links = driver.find_elements(By.XPATH, "//div[@id='top']//div[@id='main-content']//div[@class='content-block__inner']/table//tr/td[1]/h5/a")
     roles = driver.find_elements(By.XPATH, "//div[@id='top']//div[@id='main-content']//div[@class='content-block__inner']/table//tr/td[1]/p")
-    
+
     staff = []
     for staff_link, role in zip(name_links, roles):
         staff.append({"name": staff_link.text, "url": staff_link.get_attribute("href"), "role": role.text})
     
+    driver.close()
     return(staff)
 
 def clean_staff(staff_list):
@@ -37,62 +44,108 @@ def clean_staff(staff_list):
         staff["name"] = staff["name"].strip()
         staff["role"] = staff["role"].replace("\n", " ")
         staff["role"] = staff["role"].strip()
+        staff["scraped"] = False
         
         cleaned_staff_list.append(staff)
 
     return(cleaned_staff_list)
 
 def get_works_openalex(academics):
-    UniMelb_works = {}
+    UniMelb_works = []
 
     insts = Institutions().search("University of Melbourne").get()
     print(f"{len(insts)} search result(s) found for the institution")
     inst_id = insts[0]["id"].replace("https://openalex.org/", "")
 
     skipped_academics = []
-    for academic in academics:
-        auths = Authors().search(academic["name"]).filter(affiliations={"institution":{"id":inst_id}}).get()
+    for academic in (a for a in academics if not a["scraped"]):
+        auths = Authors().search(academic["name"]).filter(affiliations={"institution": {"id": inst_id}}).get()
         print(f"{len(auths)} search result(s) found for {academic['name']}")
-        
+
         try:
             auth_id = auths[0]["id"].replace("https://openalex.org/", "")
+            print(f"Selected author with id: {auth_id}")
         except IndexError:
             print("Skipping due to no results")
             skipped_academics.append(academic["name"])
             continue
 
-        works = Works().filter(author={"id":auth_id}).get()
+        pager = Works().filter(author={"id": auth_id}).paginate(per_page=200)
+
+        works = []
+        for page in pager:
+            for work in page:
+                works.append(work)
+
         print(f"{len(works)} work(s) found for {academic['name']}")
 
-        auth_works = set()
-        skip_count = 0
+        # store works in dict by name for deduplication
+        auth_works = {}
+
         for work in works:
             work_id = work["id"].replace("https://openalex.org/", "")
-            
             this_work = Works()[work_id]
 
             work_name = this_work["display_name"]
             try:
                 work_source = this_work["primary_location"]["source"]["display_name"]
-                work_date = this_work["publication_date"]
-            except TypeError:
-                skip_count += 1
+                work_date = this_work["publication_date"][:4]
+                work_type = this_work["type"]
+                work_link = this_work["doi"]
+            except (TypeError, KeyError, ValueError):
                 continue
-            
-            auth_works.add((work_name, work_source, work_date))
+
+            if work_name not in auth_works:
+                # first time seeing this work name
+                auth_works[work_name] = ([work_name, work_date, work_type, work_source, work_link, academic["name"], academic["url"]])
+            else:
+                existing_source = auth_works[work_name][3]
+
+                # replace if the existing one is from SSRN
+                if existing_source == "SSRN Electronic Journal":
+                    auth_works[work_name][3] = work_source
+                    auth_works[work_name][1] = work_date
+
+        # convert back to a list
+        for work in auth_works:
+            UniMelb_works.append(auth_works[work])
         
-        UniMelb_works[academic["name"]] = auth_works
+        academic["scraped"] = True
 
-    print(f'Skipped due to no search results: {skipped_academics}')
+    print(f"Skipped due to no search results: {skipped_academics}")
+    return UniMelb_works
 
-    return(UniMelb_works)
 
-def main():
+#Cannot be run due to protections by the UniMelb site ()
+def get_works_website(academics):
+    
+    options = uc.ChromeOptions()
+    #options.add_argument(r"user-data-dir=C:\Users\jarra\temp\User Data")
+    #options.add_argument(r'--profile-directory=Default')
+    #options.add_argument("--headless=new")
+    options.add_argument("--log-level=3")
+    
+    driver = uc.Chrome(options=options)
+
+    for academic in (a for a in academics if not a["scraped"]):
+        time.sleep(random.uniform(5, 10))
+
+        driver.get(academic["url"])
+        WebDriverWait(driver, 8).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+
+        try:
+            pub_desc = driver.find_element(By.XPATH, "//section[@id='publications']//p[@class='source-sans blue']")
+        except NoSuchElementException:
+            print(f"No publications for {academic['name']} found")
+            continue
+        
+        print(pub_desc.text)
+    
+    driver.close()
+
+def scrape_UM():
     staff_list = get_staff("https://fbe.unimelb.edu.au/about/academic-staff?queries_tags_query=4895953")
     academic_list = clean_staff(staff_list)
-    UniMelb_works = get_works_openalex(academic_list)
-    
-    return(UniMelb_works)
-
-dic = main()
-print(dic)
+    UniMelb_works = get_works_openalex(academic_list[:5])
+    #get_works_website(academic_list)
+    print(UniMelb_works)
