8cf9ffcd44f9e3ba06c6acef963be29bd3b23fab
LeweiXu <leweixu@gmail.com>
2025-09-26 17:49:51 +0800

    update requirements.txt, add 404 page

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 # app/main.py
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from app.routes import router
 from app.database import Base, engine
@@ -9,13 +9,15 @@ from starlette.middleware.sessions import SessionMiddleware
 # --- DB setup ---
 Base.metadata.create_all(bind=engine)
 
-# --- App ---
-app = FastAPI()
-app.add_middleware(SessionMiddleware, secret_key="your-secret-key")
-
 # Templates (if you want to use them globally)
 templates = Jinja2Templates(directory="app/templates")
 templates.env.globals["url_for"] = lambda name, **params: app.url_path_for(name, **params)
 
-# Include all routes from routes.py
+# --- App ---
+app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key="your-secret-key")
 app.include_router(router)
+
+@app.exception_handler(404)
+async def not_found(request: Request, exc):
+    return templates.TemplateResponse("404.html", {"request": request}, status_code=404)
